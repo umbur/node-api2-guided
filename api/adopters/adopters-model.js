aafd8cbe03dafff9f19e748cebd1ebceadca4e79
umbur <work1415@gmail.com>
2021-05-27 12:29:33 -0600

    backend practice

--- api/adopters/adopters-model.js
+++ api/adopters/adopters-model.js
@@ -1,5 +1,5 @@
-const knex = require('knex');
-const config = require('../../knexfile.js');
+const knex = require("knex");
+const config = require("../../knexfile.js");
 const db = knex(config.development);
 
 module.exports = {
@@ -12,10 +12,10 @@ module.exports = {
 };
 
 function find(query) {
-  const { page = 1, limit = 2, sortby = 'id', sortdir = 'asc' } = query;
+  const { page = 1, limit = 10, sortby = "id", sortdir = "asc" } = query;
   const offset = limit * (page - 1);
 
-  const rows = db('adopters')
+  const rows = db("adopters")
     .orderBy(sortby, sortdir)
     .limit(limit)
     .offset(offset);
@@ -24,32 +24,26 @@ function find(query) {
 }
 
 function findById(id) {
-  return db('adopters')
-    .where({ id })
-    .first();
+  return db("adopters").where({ id }).first();
 }
 
 async function add(adopter) {
-  const [id] = await db('adopters').insert(adopter);
+  const [id] = await db("adopters").insert(adopter);
 
   return findById(id);
 }
 
 function remove(id) {
-  return db('adopters')
-    .where({ id })
-    .del();
+  return db("adopters").where({ id }).del();
 }
 
 function update(id, changes) {
-  return db('adopters')
-    .where({ id })
-    .update(changes, '*');
+  return db("adopters").where({ id }).update(changes, "*");
 }
 
 function findDogs(adopterId) {
-  return db('adopters as a')
-    .join('dogs as d', 'a.id', 'd.adopter_id')
-    .select('a.id', 'a.name', 'a.email', 'd.id as dog_id', 'd.name as dog_name')
-    .where({ 'a.id': adopterId });
+  return db("adopters as a")
+    .join("dogs as d", "a.id", "d.adopter_id")
+    .select("a.id", "a.name", "a.email", "d.id as dog_id", "d.name as dog_name")
+    .where({ "a.id": adopterId });
 }
